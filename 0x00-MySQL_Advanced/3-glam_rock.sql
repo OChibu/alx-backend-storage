5faf732e353df10484543c102af9ea0cddcf55f8
OChibu <beze4luv@yahoo.com>
2023-10-12 05:35:16 -0700

    Mysql advance

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
---  lists all bands with Glam rock as their main style, ranked by their longevity
-SELECT band_name AS band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan
-FROM metal_bands
-WHERE style LIKE '%Glam rock%'
+-- lists all bands with Glam rock as their main style, ranked by their longevity
+SELECT band_name,(YEAR('2022-01-01') - YEAR(formed)) + 1 AS lifespan
+FROM bands
+WHERE style = 'Glam rock'
 ORDER BY lifespan DESC;
