d430171ad945ab48cf9675d3914868228bfc026a
OChibu <beze4luv@yahoo.com>
2023-10-17 19:10:13 -0700

    SQL

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
-'''Task 12's module.
-'''
+"""
+Task 12: a python function
+"""
 from pymongo import MongoClient
 
 
 def print_nginx_request_logs(nginx_collection):
-    '''Prints stats about Nginx request logs.
-    '''
+    """
+    Prints stats about Nginx request logs.
+    """
     print('{} logs'.format(nginx_collection.count_documents({})))
     print('Methods:')
     methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
@@ -20,11 +22,12 @@ def print_nginx_request_logs(nginx_collection):
 
 
 def run():
-    '''Provides some stats about Nginx logs stored in MongoDB.
-    '''
+    """
+    Provides some stats about Nginx logs stored in MongoDB.
+    """
     client = MongoClient('mongodb://127.0.0.1:27017')
     print_nginx_request_logs(client.logs.nginx)
 
 
-if __name__ == '__main__':
+if _name_ == '_main_':
     run()
